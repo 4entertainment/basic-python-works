4db7b6b4e4e8a776526298d402c0292c47ee59e4
4entertainment <58992739+4entertainment@users.noreply.github.com>
2023-08-24 08:12:36 +0300

    Update temp3.py

--- temp3.py
+++ temp3.py
@@ -7,4 +7,29 @@ max in implementing your solution.
 """
 
 
+def minmax(data):
 
+    min = data[0]
+    max = data[0]
+
+    for i in data:
+        if i > max:
+            max = i
+
+    for j in data:
+        if j < min:
+            min = j
+
+    return min,max
+
+
+if __name__ == "__main__":
+
+    sequence = input("Enter a sequence of numbers separated by spaces: ")
+    numbers = sequence.split()
+    numbers = [int(num) for num in numbers]
+
+    result = minmax(numbers)
+
+    print("Min number:",result[0])
+    print("Max number:", result[1])
