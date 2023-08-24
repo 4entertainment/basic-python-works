39f3c1ae7da4a23a5ff765dc432fb4ba86ba1af9
4entertainment <58992739+4entertainment@users.noreply.github.com>
2023-08-24 08:25:08 +0300

    Update temp4.py

--- temp4.py
+++ temp4.py
@@ -2,3 +2,17 @@
 R-1.4 Write a short Python function that takes a positive integer n and returns
 the sum of the squares of all the positive integers smaller than n.
 """
+
+def squares(number):
+    list = []
+    for i in range(1,n+1):
+        list.append(i*i)
+
+    return list
+
+
+if __name__ == "__main__":
+    n = int(input("Give a number:"))
+
+    result = squares(n)
+    print(result)
