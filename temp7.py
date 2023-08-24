ef4303161450159f4ceb79ce318814421f40f423
4entertainment <58992739+4entertainment@users.noreply.github.com>
2023-08-24 16:15:40 +0300

    Update temp7.py

--- temp7.py
+++ temp7.py
@@ -1,4 +1,7 @@
 """
-R-1.7 Give a single command that computes the sum from Exercise R-1.6, rely-
-ing on Python’s comprehension syntax and the built-in sum function.
+R-1.7 Give a single command that computes the sum from Exercise R-1.6, 
+relying on Python’s comprehension syntax and the built-in sum function.
 """
+
+n = int(input("Give a number:"))
+print(sum(i*i for i in range(1,n+1) if i % 2 != 0))
