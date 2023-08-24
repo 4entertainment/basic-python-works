3c61a72a0cf3ff30a46b30b6ffd276b35134c467
4entertainment <58992739+4entertainment@users.noreply.github.com>
2023-08-24 08:32:23 +0300

    Update temp5.py

--- temp5.py
+++ temp5.py
@@ -1,6 +1,6 @@
 """
-R-1.5 Give a single command that computes the sum from Exercise R-1.4, rely-
-ing on Python’s comprehension syntax and the built-in sum function.
+R-1.5 Give a single command that computes the sum from Exercise R-1.4,
+relying on Python’s comprehension syntax and the built-in sum function.
 """
 n = int(input("Give a number:"))
 print(sum(i*i for i in range(1,n+1)))
