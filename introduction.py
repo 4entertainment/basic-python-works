de6b3c2e01d132cf2f0d9c6dd3137084e14fde29
4entertainment <58992739+4entertainment@users.noreply.github.com>
2023-08-28 12:44:42 +0300

    Update introduction.py

--- introduction.py
+++ introduction.py
@@ -4,3 +4,6 @@ from transformers import pipeline
 
 classifier = pipeline("sentiment-analysis")
 classifier("I've been waiting for a HuggingFace course my whole life.")
+
+
+# alemdar, yerebatan cd. 38 34110 fatih istanbul
